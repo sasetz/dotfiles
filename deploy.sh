2474331e3e626752017562dcca220533c18ccbf3
sasetz <56696451a@gmail.com>
2022-11-13 14:40:04 +0100

    Added vim-plug autoinstall

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,10 @@ backup() {
 }
 
 replace() {
+    if [[ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]]; then
+        sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    fi
     for i in ${BACKUP_LIST[@]}; do
         if [[ -d ./$i || -f ./$i ]]; then
             mkdir -p "$HOME/$(dirname "$i")"
