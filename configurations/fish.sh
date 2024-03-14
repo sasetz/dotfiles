586259034541026c38cb9cf7647e60c2ef47ae3b
putiatin <kirill.putiatin@nokia.com>
2024-03-14 12:35:35 +0100

    Improved error checking, added debug mode, added "create" option. Added fish configuration

---
+++ configurations/fish.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./default_configuration.sh 'fish' "$HOME/.config/fish" "./data/fish" "$@"
+
---
+++ data/fish/config.fish
@@ -0,0 +1,10 @@
+abbr --add ll ls -lAhX --group-directories-first --color=auto
+abbr --add on /usr/config/bin/simbedpower.sh on
+abbr --add off /usr/config/bin/simbedpower.sh off
+abbr --add tl telnet 10.1.1.2 2501
+abbr --add tsh ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no admin@10.1.1.3
+abbr --add mk ./mk
+abbr --add cat batcat
+
+set --export --prepend PATH /usr/local/timostools
+
